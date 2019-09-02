6c01f8121a18e460286c187366480c6922758b97
root <root@mas-coursebuild.ncl.ac.uk>
2019-09-02 11:41:39 +0100

    Add redacted config.php

---
+++ config.dist.php
@@ -0,0 +1,20 @@
+<?php
+###
+###  Application settings
+###
+define('APP_NAME', 'Coursebuilder LTI Tool');
+define('SESSION_NAME', 'php-coursebuilder');
+define('VERSION', '0.1.00');
+define('WEBDIR', '/lti');
+define('LTI_CONTENTDIR', WEBDIR.'/content');
+define('MODULEDIR', '#######/lti/content');
+
+###
+###  Database connection settings
+###
+define('DB_NAME', 'mysql:dbname=#######;host=#######');
+define('DB_USERNAME', '#######');
+define('DB_PASSWORD', '#######');
+define('DB_TABLENAME_PREFIX', '');
+
+?>
