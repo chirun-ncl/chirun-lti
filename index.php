7aa4bb78f6f21ad3ad22c90c64eb787a4312977f
George Stagg <george.stagg@ncl.ac.uk>
2019-09-18 09:32:53 +0100

    Make email matching case insensitive

--- index.php
+++ index.php
@@ -112,7 +112,7 @@ if ($ok && $_SESSION['isStudent']) {
 		$page = new ModuleNotSelectedPage();
 	}
 } else if($ok && !in_array($_SESSION['user_id'],AUTH_USER_IDS)
-			&& !in_array($_SESSION['user_email'],AUTH_USER_EMAILS)){
+			&& !in_array(strtolower($_SESSION['user_email']),AUTH_USER_EMAILS)){
 	$page = new LandingPage();
 } else if ($ok && $_SESSION['isStaff']) {
 	$page = new DashboardPage();
