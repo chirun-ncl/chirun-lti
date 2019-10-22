3ebd1dd88a0d01d23b651a2d2f9f4bd65e640598
George Stagg <george.stagg@ncl.ac.uk>
2019-10-22 16:32:58 +0100

    Allow non-allowed instructors to see student view if configured

--- index.php
+++ index.php
@@ -135,7 +135,11 @@ if ($ok && $_SESSION['isStudent']) {
 	}
 } else if($ok && !in_array($_SESSION['user_id'],AUTH_USER_IDS)
 			&& !in_array(strtolower($_SESSION['user_email']),AUTH_USER_EMAILS)){
-	$page = new LandingPage();
+	if (isset($selected_module)) {
+		$page = new StudentPage();
+	} else {
+		$page = new LandingPage();
+	}
 } else if ($ok && $_SESSION['isStaff']) {
 	$page = new DashboardPage();
 } else {
