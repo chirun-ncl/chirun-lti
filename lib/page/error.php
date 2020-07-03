e1c0bb33bc11e62b0506344602ea9fea35c9a602
George Stagg <george.stagg@ncl.ac.uk>
2020-07-03 09:16:48 +0100

    Remove attempt to workaround Safari using ITP API
     * The instructions shown on the error page now match the official
    Canvas documentation advice of disabling cross-site tracking prevention
    in Safari to avoid ITP blocking iframe cookies.
    
     * Switch to a heredoc for the CSS lines in ltipage.php

--- lib/page/error.php
+++ lib/page/error.php
@@ -4,16 +4,16 @@ class ErrorPage extends LTIPage {
 		$main = <<< EOD
 			<div class="justify-content-center row mt-5">
 			<div class="col-2">
-			<img width="100%" src="{$this->webdir}/images/coursebuilder_icon_512.png">
+			<img alt="Coursebuilder logo" width="100%" src="{$this->webdir}/images/coursebuilder_icon_512.png">
 			</div>
 			</div>
 			<main id="mainCBErrorContainer" role="main" class="container mt-2">
-			<noscript>
-			For full functionality of this site it is necessary to enable JavaScript.
-			</noscript>
 			<div class="row mt-3 mb-5 justify-content-center">
 				<div class="p-3 shadow bg-white rounded col-md-10 col-lg-8 text-center">
 					<h4>Starting Coursebuilder failed</h4>
+					<noscript>
+						For full functionality of this site it is necessary to enable JavaScript.
+					</noscript>
 					<p>Sorry, Coursebuilder has encountered an error loading this page. Try reloading the page or come back later.</p>
 EOD;
 		$main .= "<div class='alert alert-warning alert-dismissible pr-1' role='alert'>";
@@ -45,60 +45,6 @@ EOD;
 					</div>
 				</div>
 			</div>
-			</main>
-			<script>
-			var promise = document.hasStorageAccess();
-			promise.then(
-			  function (hasAccess) {
-				console.log('document.hasStorageAccess: '+hasAccess);
-				if(!hasAccess){
-					document.getElementById("mainCBErrorContainer").innerHTML =
-						'<div class="text-center justify-content-center row mb-5">'+
-						'<div class="col-8">'+
-						'<p>This content is provided as a Coursebuilder Item.</p>'+
-						'<button onclick="makeRequestWithUserGesture()">Click here to load the content item.</button>'+
-						'</div>'+
-						'</div>';
-				}
-			  },
-			  function (reason) {
-				// Promise was rejected.
-				console.log('document.hasStorageAccess promise rejected: '+reason);
-			  }
-			);
-			var promise = document.requestStorageAccess();
-			var helpShown = false;
-			promise.then(
-				function () {
-					//Access granted
-				},
-				function () {
-					// Promise was rejected for some reason.
-					console.log('requestStorageAssess promise rejected');
-				});
-			function makeRequestWithUserGesture() {
-				var promise = document.requestStorageAccess();
-				promise.then(
-					function () {
-						//Access granted
-						location.reload();
-					},
-					function () {
-						// Promise was rejected for some reason.
-						console.log('requestStorageAssess promise rejected');
-						if(!helpShown){document.getElementById("CBBody").innerHTML =
-							document.getElementById("CBBody").innerHTML +
-							'<div class="row">'+
-							'<div class="mt-3 col-md-12">'+
-							'<p>Please click "Allow" to grant access to cookies and website data. We require this data so that we are able to confirm who you are and that you are authorised to see the course content.</p>'+
-							'</div>'+
-							'</div>';
-							helpShown = true;
-						}
-					}
-				);
-			}
-			</script>
 EOD;
 		return $main;
 	}
