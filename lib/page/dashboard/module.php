bddb0b6b144578066b0de72952b4f4db17a6a39a
George Stagg <george.stagg@ncl.ac.uk>
2021-12-02 16:04:41 +0000

    Don't redirect module page if requesting content
    This fixes a bug where publicly available content fails to load if
    and instructor LTI launch is active

--- lib/page/dashboard/module.php
+++ lib/page/dashboard/module.php
@@ -5,7 +5,7 @@ class DashboardSelectedContentPage extends BaseDashboardContent {
 
 	public function setup($resource){
 		parent::setup($resource);
-		if(empty($this->resource->module)){
+		if(empty($_REQUEST['req_content']) && empty($this->resource->module)){
 			$_SESSION['ack_messages'] = False;
 			header('Location: index.php?dashpage=upload');
 			return;
