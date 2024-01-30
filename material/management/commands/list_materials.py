47f5db122923e992f2ce8aa8607d2f8bdc4146ac
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-01-30 09:43:53 +0000

    list_materials command shows the last-modified time of each package

--- material/management/commands/list_materials.py
+++ material/management/commands/list_materials.py
@@ -1,9 +1,15 @@
 from django.core.management.base import BaseCommand
 from material.models import ChirunPackage
 
+from datetime import datetime
+
 class Command(BaseCommand):
     help = 'Show all materials'
 
     def handle(self, *args, **options):
-        for p in ChirunPackage.objects.all():
-            print(f'{p.title} - http://localhost:9002{p.get_absolute_url()}')
+        packages = list(ChirunPackage.objects.all())
+        for p in packages:
+            p.time = datetime.fromtimestamp(p.absolute_extracted_path.stat().st_mtime)
+
+        for p in sorted(packages, key=lambda p:p.time):
+            print(f'{p.time} {p.title} - http://localhost:9002{p.get_absolute_url()}')
